b40f0526f19cbc257e6b0547060beaf98391e955
abumafrim <abumafrim@gmail.com>
2025-05-02 19:43:09 +0000

    Deploying to gh-pages from @ abumafrim/abumafrim.github.io@922b820be40b10468381491178d80571b9fbefc2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -525,13 +525,6 @@ ninja.data = [{
           window.open("https://github.com/abumafrim", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
